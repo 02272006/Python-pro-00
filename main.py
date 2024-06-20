f31bb3d3fcb4e149a40a0a8e1ec927b3a40e6504
Musfira waseem <kwasim7844>
2024-06-20 16:04:30 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# Python project_00
+
+print ("hello","world!");
+
+print (
+    "This is Musfira, A newbie in Python!"
+    );
+message =( "Nice to see ya!" );
+name = input(
+    "what is your name?"
+    );
+ptint =(
+    "{message}, {name}!"
+    );
+
+# This is just an introduction code, or you might say a (Hello world!).
+# I'm not proficient with this language but I've started to learn Python, so this is...
+# basically, mt first program in Python!
